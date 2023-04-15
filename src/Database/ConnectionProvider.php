04108e910ce1824b4d1e176a985be136ff61be7b
Sergey Shambir <sergey.shambir@ispring.com>
2023-04-15 05:59:54 +0300

    Добавил README.md и поддержку docker-compose (в основном для себя и других преподавателей)

--- src/Database/ConnectionProvider.php
+++ src/Database/ConnectionProvider.php
@@ -8,9 +8,18 @@ class ConnectionProvider
     // Создаёт объект PDO, представляющий подключение к MySQL.
     public static function connectDatabase(): \PDO
     {
-        $dsn = 'mysql:host=127.0.0.1;dbname=php_course';
-        $user = 'php-course-app';
-        $password = 'gX5t2UUbBn';
+        $dsn = self::getEnvParameter('APP_DATABASE_DSN', 'mysql:host=127.0.0.1;dbname=php_course');
+        $user = self::getEnvParameter('APP_DATABASE_USER', 'php-course-app');
+        $password = self::getEnvParameter('APP_DATABASE_PASSWORD', 'gX5t2UUbBn');
         return new \PDO($dsn, $user, $password);
+    }
+
+    private static function getEnvParameter(string $name, string $defaultValue): string
+    {
+        if ($value = getenv($name, true))
+        {
+            return $value;
+        }
+        return $defaultValue;
     }
 }
